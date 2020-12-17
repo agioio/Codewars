fb042735edb9cb5f8f35ccbfae741da385c43e69
agioio <agioio@student.wethinkcode.co.za>
2020-12-17 16:07:58 +0200

    abbreviate name

---
+++ 8 kyu/Strings/abreviate_two_word_name.py
@@ -0,0 +1,29 @@
+"""Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+Sam Harris => S.H
+
+Patrick Feeney => P.F
+    """
+
+import unittest
+
+def abbrev_name(name):
+    return f"{name.split()[0][0]}.{name.split()[1][0]}"
+
+
+class TestAbbreviateName(unittest.TestCase):
+
+    def test_abrrev_name(self):
+        self.assertEqual(abbrev_name("Sam Harris"), "S.H")
+        self.assertEqual(abbrev_name("Patrick Feenan"), "P.F")
+        self.assertEqual(abbrev_name("Evan Cole"), "E.C")
+        self.assertEqual(abbrev_name("P Favuzzi"), "P.F")
+        self.assertEqual(abbrev_name("David Mendieta"), "D.M")
+
+
+if __name__ == "__main__":
+    unittest.main()
